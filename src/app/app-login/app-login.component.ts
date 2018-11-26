048692d4b9fbab1996bbaa1a07394b5cd33660c6
Kaviarasu <kaviarasu.lakshmanan@gmail.com>
2018-11-26 21:09:52 +0000

    Registration changes

---
+++ src/app/app-login/app-login.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './app-login.component.html',
+  styleUrls: ['./app-login.component.css']
+})
+export class AppLoginComponent {
+ title = 'Kavi';
+}
